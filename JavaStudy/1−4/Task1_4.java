6afb17d75535bdc8e7f1804e19978297f5e9d997
yokoyama-1 <masshimo0917@gmail.com>
2021-11-15 22:24:44 +0900

    Update Task1_4.java
    制御文（条件分岐）修正

--- JavaStudy/1−4/Task1_4.java
+++ JavaStudy/1−4/Task1_4.java
@@ -43,12 +43,12 @@ public class Task1_4 {
 
         // ③ 「USER_RNAME」の値のみ等しい場合。
        //  定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
-         } else if (pass != (USER_PASSWORD) && name.equals(USER_NAME)) {
+         } else if ((!(pass.equals(USER_PASSWORD))) && name.equals(USER_NAME)) {
         System.out.println(CONST_MSG_ERROR_PASS); 
 
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
-         } else if (name != (USER_NAME) && pass.equals(USER_PASSWORD)) {
+         } else if ((!(name.equals(USER_NAME))) && pass.equals(USER_PASSWORD)) {
         System.out.println(CONST_MSG_ERROR_NAME); 
 
         // ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。 
